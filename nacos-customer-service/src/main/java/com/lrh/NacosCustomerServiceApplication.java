ec495da37681ba8a583565bb88ad919d3c588244
lrh <lirh@hyxt.com>
2019-09-06 22:30:46 +0800

    nacos 注册中心和服务发现

---
+++ nacos-customer-service/src/main/java/com/lrh/NacosCustomerServiceApplication.java
@@ -0,0 +1,63 @@
+package com.lrh;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+
+@SpringBootApplication
+@Slf4j
+public class NacosCustomerServiceApplication implements CommandLineRunner {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NacosCustomerServiceApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        dicvover();
+        readMenu();
+    }
+
+    @LoadBalanced
+    @Bean
+    public RestTemplate restTemplate(RestTemplateBuilder builder){
+        return builder.build();
+    }
+
+
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+
+    public void dicvover(){
+        List<ServiceInstance> instances = discoveryClient.getInstances("wait-service");
+        instances.stream().forEach(instance -> {
+            log.info("Host {} Port {}", instance.getHost(), instance.getPort());
+        });
+    }
+
+
+    public void readMenu(){
+        ResponseEntity<String> entity = restTemplate.getForEntity("http://wait-service/car/100", String.class);
+        log.info("response context {}", entity.getBody());
+
+    }
+
+
+
+}
---
+++ nacos-waiter-service/src/main/java/com/lrh/NacosWaiterServiceApplication.java
@@ -0,0 +1,13 @@
+package com.lrh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NacosWaiterServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NacosWaiterServiceApplication.class, args);
+    }
+
+}
